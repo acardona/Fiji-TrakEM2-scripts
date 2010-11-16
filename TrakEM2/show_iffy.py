5f48583b68b2b09832f71d0f447f2b536f184cdd
Mark Longair <longair@ini.phys.ethz.ch>
2010-11-16 15:05:36 +0100

    Control-click in the 3D viewer now centres the TrakEM2 display on that point

--- TrakEM2/show_iffy.py
+++ TrakEM2/show_iffy.py
@@ -4,11 +4,43 @@ from ini.trakem2.display import Treeline
 from ini.trakem2.display import Selection
 from java.awt import Color
 from javax.vecmath import Color3f
+from javax.vecmath import Point3f
 from java.util.concurrent import Executors
 from customnode import CustomTriangleMesh
 from java.util.concurrent import Callable
 from ini.trakem2.utils import M
+from ini.trakem2.display import Coordinate
 
+# Allow the user to center the TrakEM2 display on any point clicked
+# on while holding down the control key, following the example here:
+#  http://132.187.25.13/ij3d/?category=Documentation&page=devdoc/CustomBehavior
+
+from ij3d.behaviors import InteractiveBehavior
+from java.awt.event import MouseEvent
+
+class CustomBehavior(InteractiveBehavior):
+
+  def __init__(self,univ):
+    InteractiveBehavior.__init__(self,univ)
+    self.universe = univ
+
+  def doProcess(self,e):
+    if (not e.isControlDown()) or (e.getID() != MouseEvent.MOUSE_PRESSED):
+      self.super__doProcess(e)
+      return
+    picker = self.universe.getPicker()
+    content = picker.getPickedContent(e.getX(),e.getY())
+    if not content:
+      return
+    p = picker.getPickPointGeometry(content, e)
+    if not p:
+      return
+    display = Display.getFront()
+    ls = display.getLayerSet()
+    cal = ls.getCalibration()
+    l = ls.getNearestLayer(p.z/cal.pixelWidth)
+    coordinate = Coordinate(p.x/cal.pixelWidth,p.y/cal.pixelHeight,l,None)
+    display.centerAt(coordinate)
 
 def createMesh(triangles, color):
   mesh = CustomTriangleMesh(triangles, color, 0)
@@ -79,3 +111,6 @@ for iffy in locs:
 
 d3d.addContent([outs.get() for outs in allOuts])
 exe.shutdown()
+
+universe = d3d.getUniverse()
+universe.setInteractiveBehavior(CustomBehavior(universe))

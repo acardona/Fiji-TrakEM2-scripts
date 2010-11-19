07d0c31eaa3abc60f9b752e4fb7c39755941ecd6
Albert Cardona <acardona@ini.phys.ethz.ch>
2010-11-19 21:00:40 +0100

    Added integrity test for a Tree subclass

---
+++ test/test_tree.py
@@ -0,0 +1,39 @@
+# Test the mysteriously disappearing nodes
+from ini.trakem2.display import Display, Tree, Layer, LayerSet
+from java.lang.reflect import Method, Field
+
+def run():
+  sel = Display.getSelected()
+  if sel is None or sel.isEmpty():
+    print "Please select a treeline or areatree!"
+    return
+  t = sel[0]
+	if not isinstance(t, Tree):
+    print "The active selected object is not a treeline or areatree!"
+		return
+  # Print data
+	layer = Display.getFrontLayer()
+	m = Tree.getDeclaredMethod("getNodesToPaint", [Layer])
+	m.setAccessible(True)
+	ndsp = m.invoke(t, [layer])
+	print "Nodes to paint:", len(ndsp)
+	print "-->", ndsp
+	fcc = LayerSet.getDeclaredField("n_layers_color_cue")
+	fcc.setAccessible(True)
+	print "color cue value:", fcc.get(layer.getParent())
+	layers = layer.getParent().getColorCueLayerRange(layer)
+	print "Layers to paint, for color cues:", len(layers)
+	print "-->", layers
+	f = Tree.getDeclaredField("node_layer_map")
+	f.setAccessible(True)
+	ndmap = f.get(t)
+	nds = ndmap.get(layer)
+	print "Nodes in the current layer:", len(nds)
+	print "-->", nds
+	print "node layer map has:", len(ndmap), "entries"
+	lm = {}
+	for la in ndmap.keySet():
+    lm[la] = [layer.getParent().getLayers().contains(la), layer.getParent().indexOf(la)]
+	print "layers found:", lm
+
+run()

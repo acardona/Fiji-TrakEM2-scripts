6a7fe0626b79e12c32564716ea99267af7e3647d
Albert Cardona <acardona@ini.phys.ethz.ch>
2010-11-19 21:06:23 +0100

    test tree: compare number of nodes when retrived from root.

--- test/test_tree.py
+++ test/test_tree.py
@@ -8,32 +8,34 @@ def run():
     print "Please select a treeline or areatree!"
     return
   t = sel[0]
-	if not isinstance(t, Tree):
+  if not isinstance(t, Tree):
     print "The active selected object is not a treeline or areatree!"
-		return
+    return
   # Print data
-	layer = Display.getFrontLayer()
-	m = Tree.getDeclaredMethod("getNodesToPaint", [Layer])
-	m.setAccessible(True)
-	ndsp = m.invoke(t, [layer])
-	print "Nodes to paint:", len(ndsp)
-	print "-->", ndsp
-	fcc = LayerSet.getDeclaredField("n_layers_color_cue")
-	fcc.setAccessible(True)
-	print "color cue value:", fcc.get(layer.getParent())
-	layers = layer.getParent().getColorCueLayerRange(layer)
-	print "Layers to paint, for color cues:", len(layers)
-	print "-->", layers
-	f = Tree.getDeclaredField("node_layer_map")
-	f.setAccessible(True)
-	ndmap = f.get(t)
-	nds = ndmap.get(layer)
-	print "Nodes in the current layer:", len(nds)
-	print "-->", nds
-	print "node layer map has:", len(ndmap), "entries"
-	lm = {}
-	for la in ndmap.keySet():
+  layer = Display.getFrontLayer()
+  m = Tree.getDeclaredMethod("getNodesToPaint", [Layer])
+  m.setAccessible(True)
+  ndsp = m.invoke(t, [layer])
+  print "Nodes to paint:", len(ndsp)
+  print "-->", ndsp
+  fcc = LayerSet.getDeclaredField("n_layers_color_cue")
+  fcc.setAccessible(True)
+  print "color cue value:", fcc.get(layer.getParent())
+  layers = layer.getParent().getColorCueLayerRange(layer)
+  print "Layers to paint, for color cues:", len(layers)
+  print "-->", layers
+  f = Tree.getDeclaredField("node_layer_map")
+  f.setAccessible(True)
+  ndmap = f.get(t)
+  nds = ndmap.get(layer)
+  print "Nodes in the current layer:", len(nds)
+  print "-->", nds
+  nds2 = [nd for nd in t.getRoot().getSubtreeNodes() if nd.getLayer() == layer]
+  print "from root, there should be:", len(nds2)
+  print "node layer map has:", len(ndmap), "entries"
+  lm = {}
+  for la in ndmap.keySet():
     lm[la] = [layer.getParent().getLayers().contains(la), layer.getParent().indexOf(la)]
-	print "layers found:", lm
+  print "layers found:", lm
 
 run()

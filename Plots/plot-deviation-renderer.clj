6f695f476077928da792e3a8b2214a714471d6a6
Albert Cardona <acardona@ini.phys.ethz.ch>
2011-11-29 08:13:04 -0500

    plot-deviation-renderer.clj got a GenericDialogPlus and minor fixes.

--- Plots/plot-deviation-renderer.clj
+++ Plots/plot-deviation-renderer.clj
@@ -1,3 +1,9 @@
+; Albert Cardona 2011-11-29
+; Give a CSV file, load sets of 3 columns as x,y,r, where r is
+; painted a colored band around the line defined by x,y.
+; Look at the bottom for an example.
+
+
 (ns my.plot
   (:import [java.io BufferedReader InputStreamReader FileInputStream]
            [java.awt Color Dimension BasicStroke]
@@ -6,7 +12,8 @@
            [org.jfree.chart.plot XYPlot PlotOrientation]
            [org.jfree.chart.renderer.xy DeviationRenderer]
            [org.jfree.data.xy XYDataset YIntervalSeries YIntervalSeriesCollection]
-           [org.jfree.ui RectangleInsets]))
+           [org.jfree.ui RectangleInsets]
+           [fiji.util.gui GenericDialogPlus]))
 
 (defmacro read-lines
   [filename f]
@@ -21,6 +28,7 @@
   (try
     (Double/parseDouble (.trim text))
     (catch NumberFormatException nfe
+      (println "Using 0 for cell: " text)
       default-value)))
 
 (defn parse-csv
@@ -68,7 +76,7 @@
   [filename & options]
   (let [opts (when options (apply assoc {} options))
         data (as-columns (parse-csv filename opts))
-        series (or (:series opts) {:x 0 :y 1 :r 2})
+        series (or (:series opts) [{:x 0 :y 1 :r 2}])
         dataset (YIntervalSeriesCollection.)]
     (doseq [s series]
       (.addSeries dataset (as-y-interval-series data s)))
@@ -93,17 +101,17 @@
     (.setRenderer plot renderer)
     (dotimes [i (count series)]
       (let [s (nth series i)]
-        (.setSeriesStroke renderer (int i) (BasicStroke. (float (:stroke s)) 1 1))
-        (.setSeriesFillPaint renderer (int i) (apply #(Color. %1 %2 %3 %4) (:color s)))))
+        (.setSeriesStroke renderer (int i)
+                          (BasicStroke. (float (or (:stroke s) 1)) 1 1))
+        (.setSeriesFillPaint renderer (int i)
+                             (apply #(Color. %1 %2 %3 %4) (or (:color s) [255 0 0 255])))))
     (doto (.getRangeAxis plot)
-      (.setAutoRangeIncludesZero false)
-      ;(.setStandardTickUnits (NumberAxis/createIntegerTickUnits))
-      )
+      (.setAutoRangeIncludesZero false))
     chart))
 
 (defn plot
   [filename & options]
-  (let [opts (when options (apply assoc {} options))]
+  (let [opts (apply assoc {} options)]
     (doto (JFrame. "Plot")
       (.setContentPane
         (doto (ChartPanel.
@@ -117,14 +125,47 @@
       (.pack)
       (.setVisible true))))
 
+(comment
 (plot "/home/albert/lab/Marta/crabspeed_data.csv"
-      :skip 2
+      :skip 2 ; Lines to skip from the top of the CSV file
       :separation ","
       :title "Title"
       :x-label "X-axis"
       :y-label "Y-axis"
       :background [255 255 255]
+      ; Colors are in RGBA
       :series [{:x 0 :y 1 :r 4 :title "First" :color [255 0 0 128] :stroke 1.0}
                {:x 7 :y 8 :r 11 :title "Second" :color [0 0 255 128] :stroke 1.0}
                {:x 14 :y 15 :r 18 :title "Third" :color [0 200 0 128] :stroke 1.0}])
+)
 
+(defn choose
+  []
+  (let [^GenericDialogPlus gd (GenericDialogPlus. "Plot CSV")]
+    (doto gd
+      (.addFileField "Choose *.csv file" "/home/albert/lab/Marta/crabspeed_data.csv" 50)
+      (.addSlider "Skip lines" 1 20 2)
+      (.addStringField "Separation" ",")
+      (.addStringField "Title" "Plot" 50)
+      (.addStringField "X-axis label" "X-axis" 50)
+      (.addStringField "Y-axis label" "Y-axis" 50)
+      (.addStringField "Background color" "[255 255 255]" 11)
+      (.addMessage "Series (one per row):")
+      (.addTextAreas (str
+                       "{:x 0 :y 1 :r 4 :title \"First\" :color [255 0 0 128] :stroke 1.0}\n"
+                       "{:x 7 :y 8 :r 11 :title \"Second\" :color [0 0 255 128] :stroke 1.0}\n"
+                       "{:x 14 :y 15 :r 18 :title \"Third\" :color [0 200 0 128] :stroke 1.0}")
+                     nil
+                     5 80)
+      (.showDialog))
+    (when-not (.wasCanceled gd)
+      (plot (.getNextString gd)
+            :skip (int (.getNextNumber gd))
+            :separation (.getNextString gd)
+            :title (.getNextString gd)
+            :x-label (.getNextString gd)
+            :y-label (.getNextString gd)
+            :background (eval (read-string (.getNextString gd)))
+            :series (eval (read-string (str \[ (.getText (.getTextArea1 gd)) \])))))))
+
+(choose)
